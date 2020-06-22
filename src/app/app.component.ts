f431be92cb289a4c05f08dd3d48046e84e565764
nttd-satok <sato.k@isoroot.jp>
2020-06-22 20:25:01 +0900

    4 files modified, 5 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { todoList } from './todoList';
+
+import { Todo } from './todo';
+
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -7,4 +12,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   title = 'My todo-list';
+  todoList: Todo[] = [...todoList];
 }
